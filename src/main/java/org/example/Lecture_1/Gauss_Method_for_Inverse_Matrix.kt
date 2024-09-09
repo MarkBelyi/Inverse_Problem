972dcb6140334fccfe39739784bec654c3762869
MarkBelyi <123000957+MarkBelyi@users.noreply.github.com>
2024-09-09 13:22:37 +0500

    Initial commit

---
+++ src/main/java/org/example/Lecture_1/Gauss_Method_for_Inverse_Matrix.kt
@@ -0,0 +1,9 @@
+package org.example.Lecture_1
+
+fun inverseMatrix(matrix: Array<DoubleArray>): Array<DoubleArray>? {
+    val size = matrix.size
+    val augmentedMatrix = Array(size) {DoubleArray(2 * size)}
+
+
+    return null
+}
